6458d59e824d79560d9155a9fc2e5d6e79e1e035
Andreas Richter <ar@anrichter.net>
2004-07-27 15:02:07 +0000

    - start of development ;)

---
+++ src/qsvn.h
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Andreas Richter                                 *
+ *   ar@oszine.de                                                          *
+ *   http://www.oszine.de                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef QSVN_H
+#define QSVN_H
+
+#include "qsvndlg.h"
+
+class QSvn : public QSvnDlg
+{
+    Q_OBJECT
+public:
+    QSvn( QWidget *parent = 0, const char *name = 0 );
+    ~QSvn();
+};
+
+#endif
+
---
+++ src/src.pro
@@ -0,0 +1,13 @@
+TEMPLATE	= app
+CONFIG     	+= release \
+		   warn_on \
+		   thread
+TARGET 		= qsvn
+DESTDIR 	= ../bin
+SOURCES 	+= main.cpp \
+		   qsvn.cpp
+HEADERS 	+= qsvn.h
+FORMS 		+= qsvndlg.ui
+
+MOC_DIR 	= .moc
+OBJECTS_DIR 	= .obj
