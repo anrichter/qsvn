ba86745ea3ad727331417f28b88a62b758a94ef4
Andreas Richter <ar@anrichter.net>
2005-01-05 07:29:05 +0000

    - tagged version_0.0.1 of htdocs

---
+++ version_0.0.1/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+   <title>qsvn - Qt based Subversion Client</title>
+</head>
+<body>
+
+<!-- content -->
+<p>
+<p>
+<center>
+<h2>Welcome to QSvn</h2>
+<p>
+  QSvn provide a cross plattform GUI for Subversion</a>.
+</p>
+</center>
+
+<p>
+  QSvn is a cross platform GUI for the grandious new Version Control System <a href="http://subversion.tigris.org/">Subversion</a>.
+  We uses the cross-platform C++ application framework <a href="http://www.trolltech.com/products/qt/index.html">Qt</a> from Trolltech.
+  So we can provide QSvn for all Qt supported plattforms such as X11, Windows and Mac OS X.
+</p>
+<p>
+  The project is in an early phase so we don't have any contents for this site and don't have released one version of QSvn.<br />
+  If you want to contribute some work to the QSvn project you can take contact over our <a href="https://developer.berlios.de/mail/?group_id=2312">MailingLists</a>.<br />
+  For more information please visit our <a href="https://developer.berlios.de/projects/qsvn/">QSvn Project Page</a>.
+</p>
+<p>
+  Take a look at our <a href="http://svn.berlios.de/viewcvs/qsvn/trunk/src/ChangeLog?view=auto">ChangeLog</a> and you see what is done.
+  If you wish to contribute some work you can pick one open issue from <a href="http://svn.berlios.de/viewcvs/qsvn/trunk/src/doc/Todo?view=auto">Todo</a>.
+</p>
+<p>
+</p>
+<center>
+<p>The project is hosted by<br>
+<a href="http://developer.berlios.de">
+<img src="http://developer.berlios.de/bslogo.php?group_id=0&type=1" width="124" height="32" border="0" alt="BerliOS Logo"></A>
+</center>
+
+<p>&nbsp;
+<!-- end content -->
+
+</body>
+</html>
