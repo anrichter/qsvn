48a35f25187457e96b684e624270f21f54013af4
Andreas Richter <ar@anrichter.net>
2007-09-16 21:23:21 +0000

    - #9 prepare show path properties

---
+++ src/pathproperties.h
@@ -0,0 +1,40 @@
+/***************************************************************************
+ *   This file is part of QSvn Project http://ar.oszine.de/projects/qsvn   *
+ *   Copyright (c) 2004-2007 Andreas Richter <ar@oszine.de>                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License Version 2        *
+ *   as published by the Free Software Foundation.                         *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef PATHPROPERTIES_H
+#define PATHPROPERTIES_H
+
+//QSvn
+#include "ui_pathproperties.h"
+
+//Qt
+#include <QDialog>
+
+
+class PathProperties : public QDialog, public Ui::PathProperties
+{
+    Q_OBJECT
+public:
+    PathProperties(QObject *parent = 0);
+
+    ~PathProperties();
+
+};
+
+#endif
