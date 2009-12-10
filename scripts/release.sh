5b42f95233fd6b17ba5e6b4bcf2df9f6acbbec74
Andreas Richter <ar@anrichter.net>
2009-12-10 13:55:31 +0000

    Let's call release.sh from everywhere you want.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 echo "Build QSvn for Release"
-cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE="Release" ../src
+cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE="Release" $(dirname $0)/../src
 make
 ctest
 
